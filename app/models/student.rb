7d5ba0149184e43ea1ec5bffe7a32da977ec2f74
github username <github email address>
2019-09-08 18:59:41 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,31 @@
 class Student
+    attr_accessor :first_name
+    @@all = []
 
+    def initialize(first_name)
+        @first_name = first_name
+
+        @@all << self
+    end
+#student, test_name, test_status, instructor
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def grade_percentage
+        student_tests= BoatingTest.all.find_all{|test| test.student.first_name == self.first_name}
+        total_tests= student_tests.length.to_f
+        total_passed= student_tests.find_all{|test| test.test_status == "passed"}
+        num_passed= total_passed.length.to_f
+        percent= (num_passed/total_tests)*100
+    end 
+
+    def self.find_student(name)
+        all.find{|student|student.first_name == name }
+    end 
+
+    def self.all
+        @@all
+    end 
+    
 end
